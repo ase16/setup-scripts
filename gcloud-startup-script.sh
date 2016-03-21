2f7253ecd51179f8f6ff34807732b5a0197a5687
Andreas Albrecht <albrecht@iodir.com>
2016-03-21 09:04:48 +0100

    updated script

--- gcloud-startup-script.sh
+++ gcloud-startup-script.sh
@@ -5,7 +5,7 @@
 # This script is run as root after boot.
 
 
-SCRIPTS_DIR = "setup-scripts"
+$SCRIPTS_DIR = "setup-scripts"
 
 echo "Update system..."
 apt-get update -y
@@ -14,13 +14,25 @@ apt-get upgrade -y
 echo "Install git..."
 apt-get install -y git
 
-# remove scripts folder if it already exists.
-if [ -d "$SCRIPTS_DIR" ]; then
-    echo "Remove scripts folder."
-    rm -rf $SCRIPTS_DIR
-fi
-git clone https://github.com/ase16/setup-scripts.git
+function cloneScriptsDir() {
+    # go to home dir of user
+    cd ~/
+    
+    # remove scripts folder if it already exists.
+    if [ -d "$SCRIPTS_DIR" ]; then
+        echo "Remove scripts folder."
+        rm -rf $SCRIPTS_DIR
+    fi
+    
+    # clone fresh copy from github
+    git clone https://github.com/ase16/setup-scripts.git
+}
 
-# run the initialization script
-/bin/su - ase16 -c "setup-scripts/hello.sh"
+export -f cloneScriptsDir
+# run clone function (as other user)
+/bin/su ase16 -c "bash -c cloneScriptsDir"
+# run hello script (as other user)
+/bin/su - ase16 -c "./setup-scripts/hello.sh"
+exit 0
+
 
