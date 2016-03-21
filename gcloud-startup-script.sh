621fb0d3effe97b59e5a05aa16a741cb8e2a58f0
Andreas Albrecht <albrecht@iodir.com>
2016-03-21 08:42:46 +0100

    simple startup script

---
+++ gcloud-startup-script.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# Documentation: https://cloud.google.com/compute/docs/startupscript?hl=en_US
+
+# This script is run as root after boot.
+
+
+SCRIPTS_DIR = "setup-scripts"
+
+echo "Update system..."
+apt-get update -y
+apt-get upgrade -y
+
+echo "Install git..."
+apt-get install -y git
+
+# remove scripts folder if it already exists.
+if [ -d "$SCRIPTS_DIR" ]; then
+    echo "Remove scripts folder."
+    rm -rf $SCRIPTS_DIR
+fi
+git clone https://github.com/ase16/setup-scripts.git
+
+# run the initialization script
+/bin/su - ase16 -c "setup-scripts/hello.sh"
+
