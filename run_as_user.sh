1341af1413af668bfcd8aa63c82cafe08d7ee282
Andreas Albrecht <albrecht@iodir.com>
2016-04-17 00:33:28 +0200

    setup geoffrey project, start worker

--- run_as_user.sh
+++ run_as_user.sh
@@ -4,4 +4,9 @@ echo "Run as user..."
 # make sure we work in the home directory!
 cd ~/
 
-git clone https://github.com/ase16/twitter-fetcher.git
+git clone https://github.com/ase16/geoffrey.git
+cd geoffrey
+curl "http://metadata.google.internal/computeMetadata/v1/project/attributes/config-production" -H "Metadata-Flavor: Google" > config/production.json
+npm install
+export NODE_ENV=production
+nohup node worker.js > worker.log &
