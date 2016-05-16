fa428c13756ccb35008cab9b068efa6c78ae0e53
Andreas Albrecht <albrecht@iodir.com>
2016-05-16 10:20:59 +0200

    install build essential

--- geoffrey/run_as_root.sh
+++ geoffrey/run_as_root.sh
@@ -5,3 +5,6 @@ echo "Geoffrey: Run as root..."
 echo "Geoffrey: Install current nodejs..."
 curl -sL https://deb.nodesource.com/setup_5.x | bash -
 apt-get install -y nodejs
+
+echo "Geoffrey: Install build-essential..."
+apt-get install -y build-essential
